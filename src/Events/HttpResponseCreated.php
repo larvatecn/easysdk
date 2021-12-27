270aad5a7686757af4699a8af96dd31af8706ca9
Tongle Xu <xutongle@msn.com>
2021-12-27 23:55:23 +0800

    事件

--- src/Events/HttpResponseCreated.php
+++ src/Events/HttpResponseCreated.php
@@ -9,6 +9,25 @@ declare(strict_types=1);
  */
 namespace Larva\EasySDK\Events;
 
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class HttpResponseCreated.
+ *
+ * @author mingyoung <mingyoungcheung@gmail.com>
+ */
 class HttpResponseCreated
 {
+    /**
+     * @var ResponseInterface
+     */
+    public $response;
+
+    /**
+     * @param ResponseInterface $response
+     */
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
+    }
 }
