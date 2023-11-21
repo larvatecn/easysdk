4cd5f298b64703dfae80276337f8f09d18b75af6
XuTongle <xutongle@msn.com>
2023-11-21 09:27:33 +0800

    fix code style

--- src/Providers/HttpClientServiceProvider.php
+++ src/Providers/HttpClientServiceProvider.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Larva\EasySDK\Providers;
 
 use GuzzleHttp\Client;
